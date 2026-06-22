894fdd2d78023ccbe124d7ad484d52d308f97f9d
Aman kabra <amankabra.it24@gmail.com>
2026-06-22 09:44:26 +0530

    add website

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -2,5 +2,5 @@
 // ⬇️ Set this to your deployed backend URL (e.g. https://your-app.back4app.io)
 export const environment = {
   production: true,
-  apiUrl: 'https://YOUR-BACKEND-URL',
+  apiUrl: 'https://vendormanagement-qwzydmup.b4a.run/',
 };
